ebfceba1e2e46632ba11494ea3c44cba8a656efb
Supachok <outhailnw@gmail.com>
2025-04-30 20:21:20 +0700

    add Favorite view and Log view

---
+++ CraftBeer/Beer/Views/Favorites/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  CraftBeer
+//
+//  Created by Supachok Chatupamai on 30/4/2568 BE.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
---
+++ CraftBeer/Beer/Views/Log/MyLogView.swift
@@ -0,0 +1,18 @@
+//
+//  MyLogView.swift
+//  CraftBeer
+//
+//  Created by Supachok Chatupamai on 30/4/2568 BE.
+//
+
+import SwiftUI
+
+struct MyLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyLogView()
+}
